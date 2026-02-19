97edbbbb315fbeb14a3a6519f388396707c40d35
jestiii <jestinethomas556@gmail.com>
2026-02-19 19:15:36 +0530

    added

--- src/CustomException.java
+++ src/CustomException.java
@@ -10,7 +10,7 @@ public class CustomException {
 
     static void validate(int age) throws InvalidAgeException {
         if (age < 18) {
-            throw new InvalidAgeException("Not eligible to vote");
+            throw new InvalidAgeException("Exception: Not eligible to vote");
         } else {
             System.out.println("Eligible to vote");
         }
